4fac353c8b0244f4df5b4855607859420161c9b9
RuFFinw <ShkliarVlad@gmail.com>
2025-11-04 05:23:50 +0300

    Добавил Яндекс карту через API

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ document
         document.getElementById("sliderCut").style.clipPath 
             = `inset(0 0 0 ${Number(change.target.value)}px)`
     })
+
+
+
+function initMap() {
+    const coordinates = [59.938656, 30.323058];
+    const mapCenter = [59.938439, 30.319246];
+    const myMap = new ymaps.Map('yandex-map', {
+        center: mapCenter,
+        zoom: 16,
+        controls: ['zoomControl']
+    });
+    
+    const backgroundPlacemark = new ymaps.Placemark(coordinates, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(`
+            <svg width="34" height="44" viewBox="0 0 34 44" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17 1C25.84 1 33 8.16 33 17C33 25.84 17 43 17 43C17 43 1 25.84 1 17C1 8.16 8.16 1 17 1Z" 
+                    fill="white" stroke="#e6e6e6" stroke-width="1.5"/>
+            </svg>
+        `),
+        iconImageSize: [70, 70],
+        iconImageOffset: [-30, -60] 
+    });
+    
+    const logoPlacemark = new ymaps.Placemark(coordinates, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: './assets/icon-logo.svg',
+        iconImageSize: [60, 60],
+        iconImageOffset: [-25.5, -65]
+    });
+
+    myMap.geoObjects.add(backgroundPlacemark);
+    myMap.geoObjects.add(logoPlacemark); 
+}
+
+ymaps.ready(initMap);
